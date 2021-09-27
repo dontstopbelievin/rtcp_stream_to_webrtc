ab8c59b87e3bdd3e8579fb67147a3bf8d45c4b84
aman <amanbek94@gmail.com>
2021-09-27 16:21:38 +0600

    added htts sever:

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const express = require('express')
 const cors = require('cors')
 const Stream = require('node-rtsp-stream')
+var https = require('https'); // require native node's native https module
+var fs = require('fs');
+
+var privateKey  = fs.readFileSync('./certs/praetorium.loc.pem', 'utf8');
+var certificate = fs.readFileSync('./certs/praetorium.loc.crt', 'utf8');
+
+var credentials = {key: privateKey, cert: certificate};
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
-const port = 3001
+// const port = 3001
+const https_port = 8086
 const wsPort = 9997
 
 let stream = null
@@ -48,6 +56,12 @@ app.post('/start', function (req, res) {
   }
 })
 
-app.listen(port, () => {
-  console.log(`server listening commands at http://praetorium.loc:${port}`)
-})
+var serverSecured = https.createServer(credentials, app);
+
+httpsServer.listen(https_port, () => {
+  console.log(`server listening commands at https://praetorium.loc:${https_port}`)
+});
+
+// app.listen(https_port, () => {
+//   console.log(`server listening commands at http://praetorium.loc:${port}`)
+// })
